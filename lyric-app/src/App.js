1bf31f3189f9a9e57b6426a94d97a7eb0c7209c7
sinothandomabhena <sinothandomabhena@gmail.com>
2020-12-18 13:29:07 +0200

    //

--- lyric-app/src/App.js
+++ lyric-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layouts/Navbar'
+import Index from './components/layouts/Index'
+import {Provider} from './context'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
